99b7ef4e1a1cb84e3265a5648a87918f3dd14e5a
Yashwanth-Hash <183641464+Yashwanth-hash@users.noreply.github.com.>
2026-08-05 23:49:02 +0530

    house rubber

---
+++ Dp/House_robber.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+
+    public int f(int ind, int[] nums, int[] dp) {
+
+        if (ind == 0)
+            return nums[0];
+
+        if (ind < 0)
+            return 0;
+
+        if (dp[ind] != -1)
+            return dp[ind];
+
+        int pick = nums[ind] + f(ind - 2, nums, dp);
+        int notPick = f(ind - 1, nums, dp);
+
+        dp[ind] = Math.max(pick, notPick);
+
+        return dp[ind];
+    }
+
+    public int rob(int[] nums) {
+
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return f(n - 1, nums, dp);
+    }
+}
